6886f07d32cfb15b491716608d1183b7002cccfc
Jeroen Meijer <jjgmeijer@gmail.com>
2018-10-04 12:54:46 +0200

    Fix slurm

--- src/main/resources/slurm.sh
+++ src/main/resources/slurm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-problems=`seq 1 9`
+problems=$(seq 1 9)
 learners=(ADT DHC DiscriminationTree KearnsVazirani ExtensibleLStar MalerPnueli RivestSchapire TTT)
 auts=("-M" "-B" "-M -B")
 bbos=("" "-C" "-D")
@@ -17,7 +17,8 @@ for problem in $problems; do
 				name="problem-$problem-$learner-$aut-$bbo"
 				name=${name// /_}
 				file="$LOG/$name"
-				srun "-J$name" -t90 -N1 -c7 --output="$file.csv" --error="$file.log" java "nl.utwente.fmt.rers.Main" "-c" "-t60" "$problem" "$learner" $aut $bbo &
+				multiplier=$((5 * problem))
+				srun "-J$name" -N1 -c7 -t300 --output="$file.csv" --error="$file.log" java "nl.utwente.fmt.rers.Main" "-c" "-t3600" "-m$multiplier.0" "-u20" "$problem" "$learner" $aut $bbo &
 			done;
 		done
 	done
